4659d7d41a449b2247cb2ec213f635e68e8f0dad
x5se7en9x <tnansci@gmail.com>
2020-06-15 12:50:37 -0400

    Refactored in App and Canvas

--- src/components/Canvas.jsx
+++ src/components/Canvas.jsx
@@ -1,52 +1,62 @@
 import React from 'react'
+import PropTypes from 'prop-types'
 import Sky from './Sky'
 import Ground from './Ground'
 import CannonBase from './CannonBase'
 import CannonPipe from './CannonPipe'
-import PropTypes from 'prop-types'
-import CannonBall from './CannonBall'
 import CurrentScore from './CurrentScore'
 import FlyingObject from './FlyingObject'
-import Heart from './Heart'
 import StartGame from './StartGame'
 import Title from './Title'
 
 const Canvas = (props) => {
   const gameHeight = 1200
   const viewBox = [window.innerWidth / -2, 100 - gameHeight, window.innerWidth, gameHeight]
-  
+
   return (
-  <svg
-    id="aliens-go-home-canvas"
-    preserveAspectRatio="xMaxYMax none"
-    onMouseMove={props.trackMouse}
-    viewBox={viewBox}
-  >
+    <svg
+      id="aliens-go-home-canvas"
+      preserveAspectRatio="xMaxYMax none"
+      onMouseMove={props.trackMouse}
+      viewBox={viewBox}
+    >
+      <defs>
+        <filter id="shadow">
+          <feDropShadow dx="1" dy="1" stdDeviation="2" />
+        </filter>
+      </defs>
+      <Sky />
+      <Ground />
+      <CannonPipe rotation={props.angle} />
+      <CannonBase />
+      <CurrentScore score={15} />
 
-  <defs>
-    <filter id="shadow">
-      <feDropShadow dx="1" dy="1" stdDeviation="2" />
-    </filter>
-  </defs>
+      { ! props.gameState.started &&
+        <g>
+          <StartGame onClick={() => props.startGame()} />
+          <Title />
+        </g>
+      }
 
-    <Sky />
-    <Ground />
-    <CannonPipe rotation={props.angle} />
-    <CannonBase />
-    <CannonBall position={{x: 0, y: -100}}/>
-    <CurrentScore score={15} />
-    <FlyingObject position={{x: -150, y: -300}}/>
-    <FlyingObject position={{x: 150, y: -300}}/>
-    <Heart position={{x: -300, y: 35}} />
-    <StartGame onClick={() => console.log('Aliens, Go Home!')} />
-    <Title />
-  </svg>
+      { props.gameState.started &&
+        <g>
+          <FlyingObject position={{x: -150, y: -300}}/>
+          <FlyingObject position={{x: 150, y: -300}}/>
+        </g>
+      }
+    </svg>
   )
 }
 
 Canvas.propTypes = {
   angle: PropTypes.number.isRequired,
-  trackMouse: PropTypes.func.isRequired
+  gameState: PropTypes.shape({
+    started: PropTypes.bool.isRequired,
+    kills: PropTypes.number.isRequired,
+    lives: PropTypes.number.isRequired,
+  }).isRequired,
+  trackMouse: PropTypes.func.isRequired,
+  startGame: PropTypes.func.isRequired
 }
 
 export default Canvas
