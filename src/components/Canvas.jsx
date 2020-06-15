374b81b843a57a82f74402fbbe0199af308f2d34
x5se7en9x <tnansci@gmail.com>
2020-06-15 11:56:39 -0400

    Added FlyingObject component
    Added FlyingObjectBase and FlyingObjectTop and combined them into 1 object as FlyingObject

--- src/components/Canvas.jsx
+++ src/components/Canvas.jsx
@@ -5,6 +5,8 @@ import CannonBase from './CannonBase'
 import CannonPipe from './CannonPipe'
 import PropTypes from 'prop-types'
 import CannonBall from './CannonBall'
+import CurrentScore from './CurrentScore'
+import FlyingObject from './FlyingObject'
 
 const Canvas = (props) => {
   const viewBox = [window.innerWidth / -2, 100 - window.innerHeight, window.innerWidth, window.innerHeight]
@@ -17,11 +19,20 @@ const Canvas = (props) => {
     viewBox={viewBox}
   >
 
+  <defs>
+    <filter id="shadow">
+      <feDropShadow dx="1" dy="1" stdDeviation="2" />
+    </filter>
+  </defs>
+
     <Sky />
     <Ground />
     <CannonPipe rotation={props.angle} />
     <CannonBase />
     <CannonBall position={{x: 0, y: -100}}/>
+    <CurrentScore score={15} />
+    <FlyingObject position={{x: -150, y: -300}}/>
+    <FlyingObject position={{x: 150, y: -300}}/>
   </svg>
   )
 }
